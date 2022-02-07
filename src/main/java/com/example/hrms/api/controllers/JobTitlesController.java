18f5757b6ab54a74506176fb2a09fccd026676a0
Talha Arıç <talharic@gmail.com>
2022-02-07 23:33:35 +0300

    Edit JobTitle

--- src/main/java/com/example/hrms/api/controllers/JobTitlesController.java
+++ src/main/java/com/example/hrms/api/controllers/JobTitlesController.java
@@ -17,7 +17,6 @@ public class JobTitlesController {
 
     @Autowired
     public JobTitlesController(JobTitleService jobTitleService) {
-        super();
         this.jobTitleService = jobTitleService;
     }
 
