f31d4c7d4a6de517b36e3de9b5824e38e0885e3f
Talha Arıç <talharic@gmail.com>
2022-02-16 19:25:16 +0300

    add CrossOrigin to Controllers

--- src/main/java/com/example/hrms/api/controllers/JobTitlesController.java
+++ src/main/java/com/example/hrms/api/controllers/JobTitlesController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/jobTitles")
+@CrossOrigin
 public class JobTitlesController {
 
     private JobTitleService jobTitleService;
