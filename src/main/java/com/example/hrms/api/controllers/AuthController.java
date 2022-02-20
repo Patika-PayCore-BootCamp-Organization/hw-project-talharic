f198e690a2dbf6b66aef94722222e6b5122de08f
Talha Arıç <talharic@gmail.com>
2022-02-20 11:54:15 +0300

    Edit

--- src/main/java/com/example/hrms/api/controllers/AuthController.java
+++ src/main/java/com/example/hrms/api/controllers/AuthController.java
@@ -49,11 +49,9 @@ public class AuthController {
     public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions) {
 
         Map<String, String> validationErrors = new HashMap<String, String>();
-
         for (FieldError fieldError : exceptions.getBindingResult().getFieldErrors()) {
             validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
         }
-
         ErrorDataResult<Object> errors = new ErrorDataResult<Object>(validationErrors);
 
         return errors;
